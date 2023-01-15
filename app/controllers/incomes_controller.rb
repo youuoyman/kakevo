63fc6c6249ec753d9cf54e82fbb608fd1e0632e6
youuoyman <mimisuke0926@gmail.com>
2023-01-15 22:00:18 +0900

    収入科目登録・更新機能２

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -15,10 +15,23 @@ class IncomesController < ApplicationController
     @income = Income.find(params[:id])
 	end
  
-	def create
+  def create
+		@income = Income.new(params[:income])
+		if @income.save
+			redirect_to @income, notice: "収入科目を登録しました"
+		else
+			render "new"
+		end
 	end
- 
+
 	def update
+		@income = Income.find(params[:id])
+		@income.assign_attributes(params[:income])
+		if @income.save
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
  
 	def destroy

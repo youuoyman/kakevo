25738e77723bbe6936d36e782976888c1bb8799c
youuoyman <mimisuke0926@gmail.com>
2023-01-16 22:56:59 +0900

    名前変更

--- app/models/form/fixed_value.rb
+++
@@ -1,7 +0,0 @@
-class Form::FixedValue < FixedValue
-	REGISTRABLE_ATTRIBUTES = %i(fixed_id year_month value description)
-	attr_accessor :fixed_id
-	attr_accessor :year_month
-	attr_accessor :value
-	attr_accessor :description
-end

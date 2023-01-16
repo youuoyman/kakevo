a6362686e11c1021cdbd3c92b874d4878ce91a7e
youuoyman <mimisuke0926@gmail.com>
2023-01-16 11:40:44 +0900

    コントローラー設定
    収入科目とほぼ同じなのでコピー&ペースト

---
+++ app/controllers/fixeds_controller.rb
@@ -0,0 +1,51 @@
+class FixedsController < ApplicationController
+  before_action :set_fixed, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @fixeds = Fixed.order(created_at: :asc)
+	end
+ 
+	def show
+	end
+ 
+	def new
+    @fixed = Fixed.new()
+	end
+ 
+	def edit
+	end
+ 
+  def create
+		@fixed = Fixed.new(fixed_params)
+		if @fixed.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def update
+		if @fixed.update(fixed_params)
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+ 
+  def destroy
+		@fixed.destroy
+		redirect_to root_path
+	end
+
+  private
+
+  def set_fixed
+    @fixed = Fixed.find(params[:id])
+  end
+
+  def fixed_params
+    params.require(:fixed).permit(:name, :description)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'top#index'
 
   resources :incomes
+  resources :fixeds
 end
 

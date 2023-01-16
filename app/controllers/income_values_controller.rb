9e8e3e53c6253c6dafe46a4513042c395b53fc8e
youuoyman <mimisuke0926@gmail.com>
2023-01-16 15:47:27 +0900

    収入科目データ登録機能

--- app/controllers/income_values_controller.rb
+++ app/controllers/income_values_controller.rb
@@ -3,4 +3,13 @@ class IncomeValuesController < ApplicationController
     @incomes = Income.order(created_at: :asc)
     @income_value = IncomeValue.order("year_month asc")
   end
+
+  def new
+    year_month_day = params[:year_month] + "-1"
+    @year_month = year_month_day.to_date
+
+    @incomes = Income.order(created_at: :asc)
+    @form = Form::IncomeForm.new
+  end
+  
 end

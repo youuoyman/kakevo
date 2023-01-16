8f3ebe516c7736b2f65a75b9e2b54a004dbd0739
youuoyman <mimisuke0926@gmail.com>
2023-01-16 12:07:37 +0900

    モデルとマイグレーションファイル作成

---
+++ db/migrate/20230116030616_create_changes.rb
@@ -0,0 +1,9 @@
+class CreateChanges < ActiveRecord::Migration[6.0]
+  def change
+    create_table :changes do |t|
+      t.string :name, null: false
+      t.string :description
+      t.timestamps
+    end
+  end
+end

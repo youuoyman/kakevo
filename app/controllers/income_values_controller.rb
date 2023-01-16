a490c16f7400d8942e222d9986bd9c2f07df48f8
youuoyman <mimisuke0926@gmail.com>
2023-01-16 16:12:33 +0900

    収入科目データ保存と更新機能　編集

--- app/controllers/income_values_controller.rb
+++ app/controllers/income_values_controller.rb
@@ -1,4 +1,6 @@
 class IncomeValuesController < ApplicationController
+  before_action :set_income_value, only: [:edit, :update, :destroy]
+
   def index
     @incomes = Income.order(created_at: :asc)
     @income_value = IncomeValue.order("year_month asc")
@@ -13,7 +15,39 @@ class IncomeValuesController < ApplicationController
   end
 
   def edit
-    @income_value = IncomeValue.find(params[:id])
     @income = Income.find(@income_value.income_id)
   end
+
+  def create
+    @form = Form::IncomeForm.new(income_form_params)
+    if @form.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @income_value.update(income_form_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @income_value.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_income_value
+    @income_value = IncomeValue.find(params[:id])
+  end
+
+  def income_form_params
+    params.require(:form_income_form).premit(income_value_attributes: Form::IncomeValue::REGISTRABLE_ATTRIBUTES)
+  end
 end
+

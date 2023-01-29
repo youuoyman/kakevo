da9d2235667f06834017a48cb31a440fa2972da1
youuoyman <mimisuke0926@gmail.com>
2023-01-29 16:51:53 +0900

    ルーティング効率化

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root  'top#index'
-  post "income_values/new(/:name)" => "income_values#new"
-  post "fixed_values/new(/:name)" => "fixed_values#new"
-  post "change_values/new(/:name)" => "change_values#new"
+  post "income_values", to: "income_values#new"
+  post "fixed_values", to: "fixed_values#new"
+  post "change_values", to: "change_values#new"
 
   get "balance_payment" => "balance_payment#top"
   post "balnace_payment/show(/:name)" => "balance_payment#show"
-  
+
   resources :incomes
   resources :fixeds
   resources :changes
